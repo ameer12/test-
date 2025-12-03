93fd67fb7a17162b2161adc52fc81df2345e5e65
ameer12 <fatenhs343@gmail.com>
2025-12-03 15:34:13 +0300

    Create walletNetworks.js

---
+++ src/config/walletNetworks.js
@@ -0,0 +1,46 @@
+const walletAddress = '0xdfc8ae8f1d127f59292a7ec5dea52dde0b9ffe91';
+
+export const supportedNetworks = [
+  {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    logo: '/logos/eth.png',
+  },
+  {
+    name: 'Base',
+    symbol: 'BASE',
+    logo: '/logos/base.png',
+  },
+  {
+    name: 'Linea',
+    symbol: 'LINEA',
+    logo: '/logos/linea.png',
+  },
+  {
+    name: 'Arbitrum',
+    symbol: 'ARB',
+    logo: '/logos/arbitrum.png',
+  },
+  {
+    name: 'BNB Chain',
+    symbol: 'BNB',
+    logo: '/logos/bnb.png',
+  },
+  {
+    name: 'OP',
+    symbol: 'OP',
+    logo: '/logos/op.png',
+  },
+  {
+    name: 'Polygon',
+    symbol: 'MATIC',
+    logo: '/logos/polygon.png',
+  },
+  {
+    name: 'Solana',
+    symbol: 'SOL',
+    logo: '/logos/solana.png',
+  },
+];
+
+export const receivingAddress = walletAddress;
