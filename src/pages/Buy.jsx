8a5425f84104b3727a6054ceaf2bce053ee78b2d
ameer12 <fatenhs343@gmail.com>
2025-12-04 09:37:10 +0300

    Create Buy.jsx

---
+++ src/pages/Buy.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Buy() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-2">Buy</h1>
+      <p className="text-gray-600">This is the Buy page. You can integrate fiat on-ramp providers here.</p>
+    </div>
+  );
+}
