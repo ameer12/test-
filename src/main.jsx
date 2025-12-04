009266942475aefbf9c5f1e0566a5e2ce90aecf4
ameer12 <fatenhs343@gmail.com>
2025-12-04 23:11:18 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+import { WagmiConfig } from 'wagmi';
+import { Web3Modal } from '@web3modal/react';
+import { wagmiConfig, ethereumClient } from './walletConfig';
+
+const projectId = 'd98b1bfead53e9edc55ca7e5b130b220';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig config={wagmiConfig}>
+      <App />
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </WagmiConfig>
   </React.StrictMode>
-)
+);
